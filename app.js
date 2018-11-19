ae7825eaf8914787d3d4612dcb18c714c82e9b49
caropi <pisanocaroline@gmail.com>
2018-11-19 11:45:17 -0500

    Added x with event to remove data from database

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
 const cafeList = document.querySelector('#cafe-list');
-
+const form = document.querySelector('#add-cafe-form')
 // create element and render cafe
 function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross =  document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
 
     cafeList.appendChild(li);
-}
 
+
+    // Deleting data
+    cross.addEventListener('click', (e) => {
+        e.stopPropegation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
+}
+// getting data
 //reference collection and get all items inside of it - it's an asynchronous request and can't be stored in a variable
 db.collection('cafes').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     })
 });
+
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    // make these empty values so that new inputs can be added
+    form.name.value = '';
+    form.city.value = '';
+})
